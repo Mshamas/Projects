720631c2b665d17fff3cbed17ac1459c20efe47d
Mahrukh Shamas <114362646+Mshamas@users.noreply.github.com>
2025-05-30 19:41:47 -0400

    Delete 11-Docker directory

--- 11-Docker/helper.py
+++
@@ -1,8 +0,0 @@
-import lightgbm as lgb
-import pickle
-
-# Replace 'model_path' with the actual path to your model file
-# load model
-with open("model.pkl", "rb") as f:
-    model = pickle.load(f)
-    print("Model loaded successfully")
